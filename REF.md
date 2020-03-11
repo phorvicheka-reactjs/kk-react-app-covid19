6a863c5977ca8e6243d28840c8a98cb0b564e7ab
Vicheka <phorvicheka@yahoo.com>
2020-03-11 13:46:43 +0700

    yarn add papaparse, test the papaparse lib with using useState and useEffect of react, add REF.md for references links

---
+++ REF.md
@@ -0,0 +1,14 @@
+- Mastering Markdown \
+    - https://guides.github.com/features/mastering-markdown/
+
+- mholt/PapaParse \
+    - https://github.com/mholt/PapaParse?fbclid=IwAR2cWqNba4MFq5_StOkhF4jgxzVcvnyKzsOlifRnLM2QJmSWD7QrB0pbau8 \
+    - https://www.papaparse.com/ \
+    - https://www.papaparse.com/demo \
+    - https://www.papaparse.com/docs
+
+- useState set method not reflecting change immediately \
+    - https://stackoverflow.com/questions/54069253/usestate-set-method-not-reflecting-change-immediately
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Papa from 'papaparse';
+
+const App = () => {
+
+  const [data, setData] = useState(null);
+  
+  useEffect(() => {
+    Papa.parse("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-09-2020.csv", {
+      download: true,
+      header: true,
+      // rest of config ...
+      error: function(err, file, inputElem, reason)
+      {
+        // executed if an error occurs while loading the file,
+        // or if before callback aborted for some reason
+      },
+      complete: function(results) {
+        console.log("Finished:", results.data);
+        setData(results.data); 
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    console.log("data:", data);
+  }, [data]);
+
   return (
     <div className="App">
       <header className="App-header">
